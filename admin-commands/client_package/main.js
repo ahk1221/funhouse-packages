eccebd8bbbf42a25a0c39d2ffa261b15eb53a645
ahk1221 <ahk1221@teamcoremods.com>
2017-08-07 20:43:45 +0500

    Fixed font size on different resolutions.

--- admin-commands/client_package/main.js
+++ admin-commands/client_package/main.js
@@ -35,6 +35,7 @@ jcmp.events.AddRemoteCallable('show_ban_screen', (unbanDate, reason) => {
 
 function drawText(renderer, text, position, fontSize, fontName) {
     var textMetric = renderer.MeasureText(text, fontSize, fontName);
+
     renderer.DrawText(text, new Vector3f(position.x - (textMetric.x / 2), position.y, 0.5), new Vector2f(renderer.viewportSize.x, renderer.viewportSize.y), new RGBA(215, 221, 220, 255), fontSize, fontName);
 }
 
@@ -43,14 +44,15 @@ jcmp.events.Add('Render', renderer => {
         jcmp.world.SetTime(0, 0, 0);
         renderer.DrawRect(new Vector2f(0, 0), new Vector2f(renderer.viewportSize.x, renderer.viewportSize.y), new RGBA(0, 0, 0, 115));
 
-        drawText(renderer, banText, new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 9.14, 0), 45, "Impact");
+        var fontSize = Math.round(renderer.viewportSize.x / 28); 
+        drawText(renderer, banText, new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 9.14, 0), fontSize, "Impact");
 
-        drawText(renderer, "BAN REASON:", new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 5.2, 0), 45, "Impact");
+        drawText(renderer, "BAN REASON:", new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 5.2, 0), fontSize, "Impact");
 
-        drawText(renderer, reasonText.toUpperCase(), new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 3.5, 0), 45, "Impact");
+        drawText(renderer, reasonText.toUpperCase(), new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 3.5, 0), fontSize, "Impact");
 
         if(timeTillUnban !== undefined)
-            drawText(renderer, timeTillUnban, new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 2, 0), 45, "Impact");
+            drawText(renderer, timeTillUnban, new Vector3f(renderer.viewportSize.x / 2, renderer.viewportSize.y / 2, 0), fontSize, "Impact");
     }
 });
 
