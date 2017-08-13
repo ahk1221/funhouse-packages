8cec87a77dc72989e0bd30da0d1294a61a1d83bf
ahk1221 <ahk1221@teamcoremods.com>
2017-08-13 17:08:45 +0500

    Added radio change again.

--- radio/client_package/main.js
+++ radio/client_package/main.js
@@ -18,11 +18,10 @@ jcmp.events.AddRemoteCallable('recieve_file_radio', (file) => {
 
 jcmp.events.AddRemoteCallable('set_player_in_vehicle_radio', (toggle) => {
     isPlayerInVehicle = toggle;
-    jcmp.ui.CallEvent('toggle_radio_img', toggle);
+    jcmp.ui.CallEvent('toggle_radio_img', false);
     
     if(toggle == true) {
-        
-        jcmp.ui.CallEvent('do_fade_out');
+        jcmp.ui.CallEvent('dont_do_fade_out');
     }
     
     if(toggle == false) {
